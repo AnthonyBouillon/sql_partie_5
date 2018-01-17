e6a93cfa9fc444a99872408de79c773194095573
Anthony <anthony_bouillon@outlook.fr>
2018-01-17 16:28:47 +0100

    Selection sql

--- bdd.sql
+++ bdd.sql
@@ -1,31 +1,69 @@
 SQL - Partie 5 : Sélection de données
 
 Exercice 1
-Dans la table languages, afficher toutes les données de la table.
-Ligne de commande : SELECT * FROM languages
+-- Dans la table languages, afficher toutes les données de la table.
+-- Se connecte à la base et à la table en même temps
+Ligne de commande : SELECT * FROM `webDevelopment`. `languages`;
 
 Exercice 2
-Dans la table languages, afficher toutes les versions de PHP.
-Ligne de commande : SELECT * FROM languages WHERE language="PHP";
+-- Dans la table languages, afficher toutes les versions de PHP.
+Ligne de commande :
+ SELECT
+     `version` 
+FROM
+    `languages` 
+WHERE
+    language = "PHP";
 
 Exercice 3
-Dans la table languages, afficher toutes les versions de PHP et de JavaScript.
-Ligne de commande : SELECT * FROM languages WHERE language="PHP" OR language="JavaScript";
+-- Dans la table languages, afficher toutes les versions de PHP et de JavaScript.
+Ligne de commande : 
+SELECT
+    * 
+FROM
+    `languages` 
+WHERE
+    language IN ("PHP",  "Javascript");
+
 
 Exercice 4
-Dans la table languages, afficher toutes les lignes ayant pour id 3,5,7.
-Ligne de commande : SELECT * FROM languages WHERE id="3" OR id="5" OR id="7";
+-- Dans la table languages, afficher toutes les lignes ayant pour id 3,5,7.
+Ligne de commande :
+ SELECT
+    * 
+FROM 
+    `languages` 
+WHERE 
+    id IN(3,5,7);
 
 Exercice 5
-Dans la table languages, afficher les deux premières entrées de JavaScript.
-Ligne de commande : SELECT * FROM languages WHERE language LIKE 'JavaScript' LIMIT 2;
+-- Dans la table languages, afficher les deux premières entrées de JavaScript.
+Ligne de commande :
+SELECT
+     * 
+FROM
+    `languages` 
+WHERE
+    language = 'JavaScript' LIMIT 2;
 
 Exercice 6
-Dans la table languages, afficher toutes les lignes qui ne sont pas du PHP
-Ligne de commande : SELECT * FROM languages WHERE language NOT LIKE 'PHP';
+-- Dans la table languages, afficher toutes les lignes qui ne sont pas du PHP
+Ligne de commande :
+SELECT
+     * 
+FROM 
+    `languages` 
+WHERE
+     `language` <> 'PHP';
 
 Exercice 7
-Dans la table languages, afficher toutes les données par ordre alphabéthique
-Ligne de commande : SELECT * FROM languages ORBER BY language;
+-- Dans la table languages, afficher toutes les données par ordre alphabéthique
+Ligne de commande : 
+SELECT
+    * 
+FROM
+     `languages` 
+ORBER BY
+     `language` ASC;
 
--- SELECT * FROM [table] WHERE [colonne] NOT LIKE 'caractère';
+-- SELECT * FROM [table] WHERE [colonne];
